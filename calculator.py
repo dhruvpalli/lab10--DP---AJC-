9c4a68b2f051dcf64e699bdb2abe44d7fb1985bd
dhruvpalli <dhruvkpalli@gmail.com>
2025-11-16 11:11:24 -0500

    v2 changes to calc

--- calculator.py
+++ calculator.py
@@ -5,7 +5,30 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+
+import math 
+
+
 def add(a, b): 
     return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a,b):
+    return a * b 
+
+def divide(a,b):
+    if b == 0:
+        raise ZeroDivisionError("Division by zero, try again")
+    return a / b
+
+def logarithm(a,b):
+    if a <= 0 or a == 1 or b<= 0:
+        raise ValueError("invalid base for logarithm, try again")
+    return math.log(b,a)
+
+def exponent(a,b):
+    return a ** b
 
 
